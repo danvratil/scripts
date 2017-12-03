7a40bbef2a0d9c4e415e99e7059a0d40ed9f42eb
Daniel Vrátil <dvratil@kde.org>
2017-12-03 11:25:32 +0100

    Adapt to my new screen layout

--- shell/toggle-pc-tv.sh
+++ shell/toggle-pc-tv.sh
@@ -1,58 +1,46 @@
 #!/bin/sh
 
-PC_OUTPUT="DVI-I-1"
-PC_MODE="1920x1200"
+PC1_OUTPUT="DP-1"
+PC1_MODE="3840x2160"
+PC2_OUTPUT="DVI-I-1"
+PC2_MODE="1920x1200"
 PC_SINK="usb-Roland_UA-1G-00"
 PC_MIRROR_TV=0
 
 TV_OUTPUT="HDMI-0"
 TV_MODE="1920x1080"
-TV_SINK="hdmi-stereo-extra1"
+TV_SINK="hdmi-stereo-extra2"
 
 # Check which output has resolution set and consider it as active
-pc_on=$([[ -z $(xrandr | /bin/grep -E "${PC_OUTPUT} connected primary [0-9]+x[0-9]+") ]])$?
+pc1_on=$([[ -z $(xrandr | /bin/grep -E "${PC1_OUTPUT} connected primary [0-9]+x[0-9]+") ]])$?
+pc2_on=$([[ -z $(xrandr | /bin/grep -E "${PC2_OUTPUT} connected [0-9]+x[0-9]+") ]])$?
 tv_on=$([[ -z $(xrandr | /bin/grep -E "${TV_OUTPUT} connected [0-9]+x[0-9]+") ]])$?
 default_sink_idx=$(pacmd list-sinks | /bin/grep "* index:" | grep -oE "[0-9]+\$")
 default_sink=$(pactl list sinks short | /bin/grep -E "^${default_sink_idx}" | /bin/grep -oE "(${PC_SINK}|${TV_SINK})")
 
 echo "Current state:"
-echo "    PC screen: $([[ ${pc_on} -eq 1 ]] && echo on || echo off)"
+echo "    PC screen 1: $([[ ${pc1_on} -eq 1 ]] && echo on || echo off)"
+echo "    PC screen 2: $([[ ${pc2_on} -eq 1 ]] && echo on || echo off)"
 echo "    TV screen: $([[ ${tv_on} -eq 1 ]] && echo on || echo off)"
 echo "    Default sink: ${default_sink}"
 echo
 
-if [[ $pc_on == 1 && $tv_on == 0 ]]; then
+if [[ $pc1_on == 1 && $tv_on == 0 ]]; then
     # PC's on -> turn on TV, turn off PC
-    OUTPUT_ON="${TV_OUTPUT}"
-    MODE_ON="${TV_MODE}"
     SINK_ON="${TV_SINK}"
-    OUTPUT_OFF="${PC_OUTPUT}"
-elif [[ $tv_on == 1 ]]; then
-    # TV's on -> turn on PC, turn off TV
-    OUTPUT_ON="${PC_OUTPUT}"
-    MODE_ON="${PC_MODE}"
-    SINK_ON="${PC_SINK}"
-    OUTPUT_OFF="${TV_OUTPUT}"
+    xrandr --output ${PC1_OUTPUT} --off \
+           --output ${PC2_OUTPUT} --off \
+           --output ${TV_OUTPUT} --mode ${TV_MODE} || echo "Failed to change screen output"
 else
-    # PC's off and TV's off, turn on PC
-    OUTPUT_ON="${PC_OUTPUT}"
-    MODE_ON="${PC_MODE}"
+    # TV's on or both off -> turn on PC, turn off TV
     SINK_ON="${PC_SINK}"
+    xrandr --output ${TV_OUTPUT} --off \
+           --output ${PC1_OUTPUT} --mode ${PC1_MODE} --primary \
+           --output ${PC2_OUTPUT} --mode ${PC2_MODE} --right-of ${PC1_OUTPUT} --rotate left
 fi
 
 echo "New state:"
-echo "    PC screen: $([[ ${OUTPUT_ON} == ${PC_OUTPUT} ]] && echo on || echo off)"
-echo "    TV screen: $([[ ${OUTPUT_ON} == ${TV_OUTPUT} ]] && echo on || echo off)"
 echo "    Default sink: ${SINK_ON}"
-
-# Toggle screens
-xrandr --output ${OUTPUT_OFF} --off \
-       --output ${OUTPUT_ON} --mode ${MODE_ON} || echo "Failed to change screen output"
-
-if [[ ${PC_MIRROR_TV} == 1 && $tv_on == 1 ]]; then
-    xrandr --output ${PC_OUTPUT} --mode ${TV_MODE} --same-as ${TV_OUTPUT} || echo "Failed to mirror TV to PC"
-fi
-
 
 # Get index of the new sink
 sink_idx=$(pactl list sinks short | /bin/grep "${SINK_ON}" | /bin/grep -oE "^[0-9]+")
@@ -76,3 +64,4 @@ pactl list sink-inputs short | while read -r sink_input; do
 done
 
 # Done.
+
