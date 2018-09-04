785d1e42ec095bb68f891cc40072b5851b2511f2
Daniel Vrátil <dvratil@kde.org>
2018-09-04 23:40:20 +0200

    Add install.sh

---
+++ conf/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "THIS WILL DELETE FILES FROM $HOME"
+read -p "Continue? [y/N] " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+files=$(find -maxdepth 1 -type f | sed "s|^\./||")
+for file in $files; do
+    rm ~/${file}
+    ln -s $(pwd)/${file} ~/${file}
+done
+
+dirs=$(find -maxdepth 1 -type d | sed "s|^\./||")
+for dir in $dirs; do
+    rm -rf ~/${dir}
+    ln -s $(pwd)/${dir} ~/${dir}
+done
+
+if [ -d "/home/dvratil/.vim/bundle/Vundle.vim" ]; then
+    echo "Vundle for Vim already exists"
+else
+    mkdir -p ~/.vim/bundle
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
