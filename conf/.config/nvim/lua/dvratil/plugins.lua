7351b398f4b5cc7033c29a42b27d9c3b4ca045e4
Daniel Vrátil <dvratil@kde.org>
2022-09-20 16:55:07 +0200

    nvim: remove duplicated plugin

--- conf/.config/nvim/lua/dvratil/plugins.lua
+++ conf/.config/nvim/lua/dvratil/plugins.lua
@@ -35,7 +35,6 @@ require('packer').startup(function(use)
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-vsnip'
     use 'hrsh7th/vim-vsnip'
 
